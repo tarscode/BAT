cd949d7a95792324c3906f0588658e4024baa4d7
tarsdroid <tarsdroid@hotmail.com>
2016-06-14 21:35:39 +0800

    字符串移位

---
+++ src/String/Translation.java
@@ -0,0 +1,20 @@
+package String;
+
+/**
+ * Created by liuyang on 16/6/14.
+ * 字符串移位
+ */
+public class Translation {
+    public String stringTranslation(String A, int n, int len) {
+        char[] B = A.toCharArray();
+        char[] C = new char[n];
+        for(int i=0;i<n-len;i++){
+            C[i] = B[len+i];
+        }
+        for(int i=0;i<len;i++){
+            C[i+n-len] = B[i];
+        }
+        A = String.valueOf(C);
+        return A;
+    }
+}
