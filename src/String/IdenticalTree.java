15ecebde507f0377054d90d7a1a2131d9b8809b0
tarsdroid <tarsdroid@hotmail.com>
2016-07-05 23:04:34 +0800

    拓扑结构相同子树

---
+++ src/String/IdenticalTree.java
@@ -0,0 +1,87 @@
+package String;
+
+import java.util.List;
+
+/**
+ * Created by liuyang on 16/7/5.
+ * 拓扑结构相同子树
+ */
+public class IdenticalTree {
+    public boolean chkIdentical(TreeNode A, TreeNode B) {
+        StringBuffer strA = posOrderRecur(A);
+        StringBuffer strB = posOrderRecur(B);
+        String stra = strA.toString();
+        String strb = strB.toString();
+        if (getIndexOf(stra,strb)==-1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    //#!和!定位
+    public StringBuffer posOrderRecur(TreeNode head){
+        StringBuffer buffer = new StringBuffer("#!");
+        if(head == null){
+            return buffer;
+        }
+        StringBuffer buf = new StringBuffer("!");
+        buf = buf.append(head.val);
+        buf = buf.append(posOrderRecur(head.left));
+        buf = buf.append(posOrderRecur(head.right));
+        return buf;
+    }
+    //KMP算法
+    public int getIndexOf(String s, String m) {
+        if (s == null || m == null || m.length() < 1 || s.length() < m.length()) {
+            return -1;
+        }
+        char[] ss = s.toCharArray();
+        char[] ms = m.toCharArray();
+        int si = 0;
+        int mi = 0;
+        int[] next = getNextArray(ms);
+        while (si < ss.length&&mi<ms.length) {
+            if (ss[si] == ms[mi]) {
+                si++;
+                mi++;
+            } else if (next[mi] == -1) {
+                si++;
+            } else {
+                mi = next[mi];
+            }
+        }
+        return mi == ms.length ? si - mi : -1;
+    }
+
+    private int[] getNextArray(char[] ms) {
+        if (ms.length == 1) {
+            return new int[]{-1};
+        }
+        int[] next = new int[ms.length];
+        next[0] = -1;
+        next[1] = 0;
+        int pos = 2;
+        int cn = 0;
+        while (pos < next.length) {
+            if (ms[pos - 1] == ms[cn]) {
+                next[pos++] = ++cn;
+            } else if (cn > 0) {
+                cn = next[cn];
+            } else {
+                next[pos++] = 0;
+            }
+        }
+        return next;
+    }
+}
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
--- src/String/StringTest.java
+++ src/String/StringTest.java
@@ -6,10 +6,7 @@ import Util.Util;
  * Created by Tars on 2016/4/10.
  */
 public class StringTest {
-    public static void main(String args[]){
-        String[] A = {"ab","be"};
-        int n = 2;
-        Prior p = new Prior();
-        System.out.println(p.findSmallest(A,n));
+    public static void main(String args[]) {
+        String[] A = {"ab", "be"};
     }
 }
