4879b7d30554508bf65165c1557a0d99d5806d2e
tarsdroid <tarsdroid@hotmail.com>
2016-08-16 20:15:49 +0800

    链表判环

--- src/List/ChkLoop.java
+++ src/List/ChkLoop.java
@@ -3,21 +3,30 @@ package List;
 /**
  * Created by liuyang on 16/8/6.
  * 链表判环
+ * 时间:70 ms
+ * 空间:1110K
+ * 版本:1.0
  */
 public class ChkLoop {
     public int chkLoop(ListNode head, int adjust) {
-        ListNode pre;
-        ListNode node = head;
-        ListNode end = new ListNode(-1);
-        while (node != null) {
-            if (node.next != end) {
-                pre = node;
-                node = node.next;
-                pre.next = end;
-            } else {
-                return node.val;
+        if (head.next == head) {
+            return head.val;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null&&fast!=null&&fast.next!=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast.val;
             }
         }
         return -1;
     }
 }
+
