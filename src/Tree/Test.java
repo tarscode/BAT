fb9f28770a3cac66888ac19aa1f5136506fe571f
tarsdroid <tarsdroid@hotmail.com>
2016-09-15 17:30:47 +0800

    Test

--- src/Tree/Test.java
+++ src/Tree/Test.java
@@ -6,14 +6,19 @@ package Tree;
  */
 public class Test {
     public static void main(String[] args){
+        /*
         TreeNode t1 = new TreeNode(1);
-        TreeNode t2 = new TreeNode(2);
-        TreeNode t3 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(3);
+        TreeNode t3 = new TreeNode(2);
         TreeNode t4 = new TreeNode(4);
-        t1.left = t2;
-        t1.right = t3;
-        t2.left = t4;
-        CheckCompletion tt = new CheckCompletion();
-        System.out.println(tt.chk(t1));
+        TreeNode t5 = new TreeNode(5);
+        t4.left = t2;
+        t4.right = t5;
+        t2.left = t1;
+        t2.right = t3;
+        FindErrorNode tt = new FindErrorNode();
+        */
+        FoldPaper f = new FoldPaper();
+        f.foldPaper(3);
     }
 }
